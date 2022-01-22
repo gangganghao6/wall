6a1fa9cc8abca7624c3b7bc0b1fa0da567750866
gangganghao6 <530394623@qq.com>
2022-01-22 19:47:06 +0800

    cdn加速

--- front/src/main.js
+++ front/src/main.js
@@ -1,14 +1,15 @@
-import {createApp} from 'vue'
+// import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import axios from "axios";
+// import axios from "axios";
 import VueAxios from "vue-axios";
-import TDesign from 'tdesign-vue-next';
-import 'tdesign-vue-next/es/style/index.css';
+// import TDesign from 'tdesign-vue-next';
+// import 'tdesign-vue-next/es/style/index.css';
 
-axios.defaults.timeout = 5000;
-axios.defaults.baseURL = "/api";
-axios.defaults.withCredentials = true;
-
-createApp(App).use(store).use(router).use(VueAxios, axios).use(TDesign).mount('#app')
+// console.log(TDesign)
+// axios.defaults.timeout = 5000;
+// axios.defaults.baseURL = "/api";
+// axios.defaults.withCredentials = true;
+// createApp(App).use(store).use(router).use(VueAxios, axios).use(TDesign).mount('#app')
+Vue.createApp(App).use(store).use(router).use(VueAxios, axios).use(TDesign.default).mount('#app')
